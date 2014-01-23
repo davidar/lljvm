16b27f2fcbc7b58f83702fcb5e356a84939a7817
Howard Chu <hyc@symas.com>
2014-01-23 09:45:57 -0800

    llvm 3.3 compatibility
    Fix SEGV in pass initialization

--- backend/main.cpp
+++ backend/main.cpp
@@ -27,15 +27,18 @@
 #include <llvm/Analysis/Verifier.h>
 #include <llvm/Bitcode/ReaderWriter.h>
 #include <llvm/CodeGen/Passes.h>
-#include <llvm/LLVMContext.h>
+#include <llvm/IR/LLVMContext.h>
 #include <llvm/PassManager.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/FormattedStream.h>
+#include <llvm/Support/system_error.h>
 #include <llvm/Support/MemoryBuffer.h>
-#include <llvm/Target/TargetData.h>
+#include <llvm/IR/DataLayout.h>
 #include <llvm/Transforms/Scalar.h>
 
 using namespace llvm;
+
+static RegisterPass<JVMWriter> tmp("JVMWriter", "Generate java bytecode");
 
 static cl::opt<std::string> input(
     cl::Positional, cl::desc("<input bitcode>"), cl::init("-"));
@@ -55,31 +58,47 @@ cl::opt<DebugLevel> debugLevel(cl::desc("Debugging level:"), cl::init(g1),
 int main(int argc, char **argv) {
     cl::ParseCommandLineOptions(argc, argv, "LLJVM Backend\n");
     std::string err;
-    
-    MemoryBuffer *buf = MemoryBuffer::getFileOrSTDIN(input, &err);
-    if(!buf) {
-        std::cerr << "Unable to open bitcode file: " << err << std::endl;
+	OwningPtr<MemoryBuffer> buf;
+	error_code errcode;
+
+    errcode = MemoryBuffer::getFileOrSTDIN(input, buf);
+    if(errcode) {
+        std::cerr << "Unable to open bitcode file: " << errcode << std::endl;
         return 1;
     }
     
-    Module *mod = ParseBitcodeFile(buf, getGlobalContext(), &err);
+    Module *mod = ParseBitcodeFile(buf.get(), getGlobalContext(), &err);
     if(!mod) {
         std::cerr << "Unable to parse bitcode file: " << err << std::endl;
         return 1;
     }
-    
+
+    // Initialize passes
+    PassRegistry &Registry = *PassRegistry::getPassRegistry();
+    initializeCore(Registry);
+    initializeScalarOpts(Registry);
+    initializeObjCARCOpts(Registry);
+    initializeAnalysis(Registry);
+    initializeIPA(Registry);
+    initializeTransformUtils(Registry);
+    initializeInstCombine(Registry);
+    initializeTarget(Registry);
+
     PassManager pm;
-    TargetData td("e-p:32:32:32"
+    DataLayout td("e-p:32:32:32"
                   "-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64"
                   "-f32:32:32-f64:64:64");
-    pm.add(new TargetData(td));
+	JVMWriter *jvw;
+    pm.add(new DataLayout(td));
     pm.add(createVerifierPass());
     pm.add(createGCLoweringPass());
     // TODO: fix switch generation so the following pass is not needed
     pm.add(createLowerSwitchPass());
     pm.add(createCFGSimplificationPass());
-    pm.add(new JVMWriter(&td, fouts(), classname, debugLevel));
-    pm.add(createGCInfoDeleter());
+	jvw = (JVMWriter *)tmp.createPass();
+	jvw->Setup(&td, classname, debugLevel);
+    pm.add(jvw);
+//    pm.add(createGCInfoDeleter());
     pm.run(*mod);
     return 0;
 }
